4d8b622a7b88a8228c89b64e66da24e7799ebd5b
Lennart Prelle <me@prellele.de>
2013-01-25 10:24:53 +0100

    insert pjax link around tds

--- lib/event_calendar/calendar_helper.rb
+++ lib/event_calendar/calendar_helper.rb
@@ -179,7 +179,7 @@ module EventCalendar
         # day numbers row
         cal << %(<tr>)
         first_day_of_week.upto(last_day_of_week) do |day|
-          cal << %(<td data-date="#{day}" class="ec-day-header )
+          cal << %(<a href="#" data-pjax><td data-date="#{day}" class="ec-day-header )
           cal << %(ec-today-header ) if options[:show_today] and (day == Date.today)
           cal << %(ec-other-month-header ) if (day < first) || (day > last)
           cal << %(ec-weekend-day-header) if weekend?(day)
@@ -189,7 +189,7 @@ module EventCalendar
           else
             cal << %(#{day.day})
           end
-          cal << %(</td>)
+          cal << %(</td></a>)
         end
         cal << %(</tr>)
 
